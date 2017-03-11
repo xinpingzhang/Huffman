2b364748ca540b724e7921ee444993d089f0678b
Kelvin Zhang <zhangxp1998@gmail.com>
2017-03-10 23:53:07 -0500

    Finish bubble_up

--- pqueue.c
+++ pqueue.c
@@ -34,8 +34,6 @@ struct PriorityQueue
 	int       count;
 };
 
-static void bubble_down(TreeNode *arr[], int i, int end);
-
 /**
  * This function is used by the C library qsort routine.  It is similar to a
  * Comparator object in Java.  However, C uses a comparator function.  It
@@ -50,6 +48,48 @@ static int comparator (const void *x, const void *y)
 	int a = n1->freq.v;
 	int b = n2->freq.v;
 	return a - b;
+}
+
+static void bubble_down(TreeNode *arr[], int i, int end)
+{
+    assert(i >= 0);
+    assert(i < end);
+    
+    int left = LEFT_CHILD(i);
+    int right = RIGHT_CHILD(i);
+    TreeNode *src = arr[i];
+    
+    while(i < end)
+    {
+        int max = left;
+        if(comparator(arr[right], arr[left]) > 0)
+            max = right;
+        
+        if(comparator(src, arr[max]) > 0)
+            break;
+        
+        arr[i] = arr[max];
+        i = max;
+        left = LEFT_CHILD(i);
+        right = left + 1;
+    }
+    arr[i] = src;
+}
+
+static void bubble_up(TreeNode *arr[], int i)
+{
+    assert(i >= 0);
+    
+    TreeNode *src = arr[i];
+    int parent = PARENT(i);
+    
+    while (i > 0 && comparator(arr[i], arr[parent]) > 0)
+    {
+        arr[i] = arr[parent];
+        i = parent;
+        parent = PARENT(i);
+    }
+    arr[i] = src;
 }
 
 
@@ -108,7 +148,8 @@ void pqueue_enqueue (PriorityQueue *pq, TreeNode *n)
   // count against MAXSIZE).  Then, if there is room, store the tree node in
   // the array slot indicated by count, and increment count.  Finally, call
   // sort (to keep the queue properly sorted).
-
+    pq->queue[pq->count++] = n;
+    bubble_up(pq->queue, pq->count - 1);
 	return;
 }
 
@@ -140,48 +181,6 @@ TreeNode *pqueue_dequeue (PriorityQueue *pq)
     
     bubble_down(pq->queue, 0, pq->count);
 	return data;
-}
-
-static void bubble_down(TreeNode *arr[], int i, int end)
-{
-    assert(i >= 0);
-    assert(i < end);
-    
-    int left = LEFT_CHILD(i);
-    int right = RIGHT_CHILD(i);
-    TreeNode *src = arr[i];
-    
-    while(i < end)
-    {
-        int max = left;
-        if(comparator(arr[right], arr[left]) > 0)
-           max = right;
-        
-        if(comparator(src, arr[max]) > 0)
-            break;
-        
-        arr[i] = arr[max];
-        i = max;
-        left = LEFT_CHILD(i);
-        right = left + 1;
-    }
-    arr[i] = src;
-}
-
-static void bubble_up(TreeNode *arr[], int i)
-{
-    assert(i >= 0);
-    
-    TreeNode *src = arr[i];
-    int parent = PARENT(i);
-    
-    while (i > 0 && comparator(arr[i], arr[parent]) > 0)
-    {
-        arr[i] = arr[parent];
-        i = parent;
-        parent = PARENT(i);
-    }
-    arr[i] = src;
 }
 
 /**
