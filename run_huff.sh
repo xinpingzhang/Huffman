8d1cb493f98ca8fec4e0ff6a14049eb0bda90793
Kelvin Zhang <zhangxp1998@gmail.com>
2017-03-18 08:13:40 -0400

    use a better comparison script

--- run_huff.sh
+++ run_huff.sh
@@ -1,3 +1,4 @@
 ./huffc books/iliad.txt iliad.txt.he
 ./huffd iliad.txt.he iliad.txt
-vimdiff books/iliad.txt iliad.txt
+cmp --silent books/iliad.txt iliad.txt && echo '### SUCCESS: Files Are Identical! ###' || echo '### WARNING: Files Are Different! ###'
+
